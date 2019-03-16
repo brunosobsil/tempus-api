62293762cdc46babd639a99683247a861b99b19a
Bruno Calixto <b.calixto28@gmail.com>
2019-03-16 12:00:36 -0300

    Resolvendo bloqueio de requisição cross-origin

--- App.js
+++ App.js
@@ -11,6 +11,11 @@ class App {
     middlewares(){
         this.express.use(express.json());
         this.express.use(express.urlencoded({ extended: false }));
+        this.express.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
     }
 
     routes(){
