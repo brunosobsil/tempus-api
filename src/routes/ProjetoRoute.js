72b5f5358e6dcfe0fea50f6d6190910fee9a5073
LenoM <leno855@hotmail.com>
2019-06-05 02:39:34 -0300

    Exclusão de rotas obsoletas

--- src/routes/ProjetoRoute.js
+++ src/routes/ProjetoRoute.js
@@ -7,5 +7,4 @@ module.exports = function (app) {
     app.delete('/projeto/:id', controller.excluirProjeto);
     app.post('/projeto', controller.incluirProjeto);
     app.put('/projeto/:id', controller.alterarProjeto);
-    //app.get('/projeto/:data_inicio/:data_final', controller.obterHorasPorProjeto);
 }
--- src/routes/UsuarioHabilidadeRoute.js
+++ src/routes/UsuarioHabilidadeRoute.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.get('/usuario/:id_usuario/habilidade', controller.obterUsuarioHabilidade);
     app.post('/usuario/:id_usuario/habilidade', controller.incluirUsuarioHabilidade);
     app.put('/usuario/:id_usuario/habilidade/:id_habilidade', controller.alterarUsuarioHabilidade);
-    app.delete('/usuario/:id_usuario/habilidade/:id_habilidade', controller.excluirUsuarioHabilidade);    
+    app.delete('/usuario/:id_usuario/habilidade/:id_habilidade', controller.excluirUsuarioHabilidade);
     app.post('/usuario/:id_usuario/habilidades', controller.incluirUsuarioHabilidades);
-    app.delete('/usuario/:id_usuario/habilidades', controller.excluirUsuarioHabilidades);    
+    app.delete('/usuario/:id_usuario/habilidades', controller.excluirUsuarioHabilidades);
 }
