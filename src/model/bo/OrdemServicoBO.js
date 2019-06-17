fe682183a8b488df8c16030c153f82d5339e0824
Bruno Sobral <bruno.sobral.silva@gmail.com>
2019-06-16 23:37:36 -0300

    Ordem de Servico BO

--- src/model/bo/OrdemServicoBO.js
+++ src/model/bo/OrdemServicoBO.js
@@ -15,16 +15,148 @@ class OrdemServicoBO {
         return dao.obterOrdemServicoPorStatus(dt_ini, dt_fin, status);
     }
 
-    incluirOrdemServico(OrdemServico) {
-        return dao.incluirOrdemServico(OrdemServico);
+    async incluirOrdemServico(OrdemServico) {
+        
+        const error = new Array();
+
+        if (OrdemServico.agendamento) {
+            if (OrdemServico.agendamento.id == null) {
+                error.push('necessario informar o id do agendamento');
+            }
+        } else {
+            error.push('necessario informar o agendamento');
+        }
+
+        if (error.length > 0) {
+            return {
+                error: true,
+                status_code: 409,
+                status_message: 'Conflict',
+                message: error
+            }
+        } else {
+            try {
+
+                await dao.incluirOrdemServico(OrdemServico);
+
+                return {
+                    error: false,
+                    status_code: 201,
+                    message: 'Ordem de Serviço incluída com sucesso'
+                }
+            } catch (error) {
+                return {
+                    error: true,
+                    status_code: 500,
+                    status_message: 'Server Error',
+                    message: 'Erro ao incluir atendimento'
+                };
+            }
+        }
+        //return dao.incluirOrdemServico(OrdemServico);
     }
 
-    alterarOrdemServico(OrdemServico){
-        dao.alterarOrdemServico(OrdemServico);
+    async alterarOrdemServico(OrdemServico){
+        const error = new Array();
+        if (OrdemServico.id) {
+            let os = await dao.obterOrdemServico(OrdemServico);
+
+            if (os != null) {
+
+                if (os.agendamento) {
+                    if (os.agendamento.id == null) {
+                        error.push('necessario informar o id do agendamento');
+                    }
+                } else {
+                    error.push('necessario informar o agendamento');
+                }
+
+                if (error.length > 0) {
+                    return {
+                        error: true,
+                        status_code: 409,
+                        status_message: 'Conflict',
+                        message: error
+                    }
+                } else {
+
+                    try {
+                        await dao.alterarOrdemServico(OrdemServico);
+
+                        return {
+                            error: false,
+                            status_code: 200,
+                            message: 'Os atualizada com sucesso'
+                        }
+                    } catch (error) {
+                        return {
+                            error: true,
+                            status_code: 500,
+                            status_message: 'Server Error',
+                            message: 'Erro ao atualizar os'
+                        };
+                    }
+
+                }
+            } else {
+                return {
+                    error: true,
+                    status_code: 404,
+                    status_message: 'Not Found',
+                    message: 'Os não existe'
+                }
+            }
+        } else {
+            return {
+                error: true,
+                status_code: 409,
+                status_message: 'Conflict',
+                message: 'Necessario informar o id da OS'
+            };
+        }
+        //dao.alterarOrdemServico(OrdemServico);
     }
 
-    excluirOrdemServico(OrdemServico){
-        dao.excluirOrdemServico(OrdemServico);
+    async excluirOrdemServico(OrdemServico){
+        if (OrdemServico.id) {
+            let os = await dao.obterOrdemServico(OrdemServico);
+
+            if (os) {
+
+                try {
+                    await dao.excluirOrdemServico(OrdemServico);
+
+                    return {
+                        error: false,
+                        status_code: 200,
+                        message: 'OS excluida com sucesso'
+                    }
+                } catch (error) {
+                    return {
+                        error: true,
+                        status_code: 500,
+                        status_message: 'Server Error',
+                        message: 'erro ao excluir a OS'
+                    };
+                }
+            } else {
+                return {
+                    error: true,
+                    status_code: 404,
+                    status_message: 'Not Found',
+                    message: 'OS não existe'
+                }
+            }
+
+        } else {
+            return {
+                error: true,
+                status_code: 409,
+                status_message: 'Conflict',
+                message: 'necessario informar o id da OS'
+            };
+        }
+        //dao.excluirOrdemServico(OrdemServico);
     }
 
     alterarStatusOrdemServico(OrdemServico){
